71585b072b5fb66b6ee0b0ffabb96e19313f6c65
Fazail Moez <fazailmoez@gmail.com>
2026-07-13 01:26:08 +0600

    Add function_sum program

---
+++ 01 Functions/function_sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int Sum(int m, int n)
+{
+    int ans = m+n;
+    return ans;
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter 2 numbers :";
+    cin>>a>>b;
+    cout<<Sum(a,b);
+    
+}
