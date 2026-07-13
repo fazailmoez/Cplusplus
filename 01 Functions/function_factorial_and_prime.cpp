672d321924287db08af2c0253ea72f0e79f0a7ef
Fazail Moez <fazailmoez@gmail.com>
2026-07-13 15:48:53 +0600

    function_factorial_and_prime

---
+++ 01 Functions/function_factorial_and_prime.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool Prime(int n)
+{
+    if(n<2)
+    return 0;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        return 0;
+    }
+    return 1;
+}
+
+ int Fact(int n)
+ {
+    int ans=1;
+    for(int i=1;i<=n;i++)
+    {
+        ans=ans*i;
+ }
+        return ans;
+
+ }
+
+
+
+
+        int main()
+ {
+
+    int a,b;
+    cout<<"Enter the number :";
+    cin>>a>>b;
+    cout<<Prime(a)<<endl;
+    cout<<Fact(a)<<endl;
+    cout<<Prime(b)<<endl;
+    cout<<Fact(b)<<endl;
+    cout<<Prime(b-a)<<endl;
+    cout<<Fact(b-a)<<endl;
+
+ }
