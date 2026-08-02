cae89e12b659fdc8f0977531d4272bd777d8ec6e
Fazail Moez <fazailmoez@gmail.com>
2026-08-02 12:34:52 +0600

    Add getter with pin program

---
+++ 04 OOP/getter_with_pin.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+    private:
+    string name,grade;
+    int age,roll_number;
+    
+    public:
+    void setname(string s)
+    {
+        name=s;
+    }
+    
+     void setgrade(string s)
+    {
+        grade=s;
+        
+    } void setage(int a)
+    {
+        age=a;
+        
+    } void setroll_number(int r)
+    {
+        roll_number=r;
+    }
+    
+    
+    void getname()
+    {
+        cout<<"Name:"<<name<<endl;
+    }
+   
+    void getage()
+    {
+        cout<<"Age:"<<age<<endl;
+    }
+    
+    void getroll_number()
+    {
+       cout<<"Roll Number:" <<roll_number<<endl;
+    }
+    
+    string get_grade(int pin)
+{
+    if(pin==4321)
+    {
+       
+        return grade;
+    }
+    return " ";
+}    
+    
+    
+};
+
+int main()
+{
+   Student S1;
+   
+   S1.setname("Rohit");
+   S1.setgrade("A+");
+   S1.setage(20);
+   S1.setroll_number(1234);
+   
+    
+    S1.getname();
+   cout<< S1.get_grade(431)<<endl;
+    S1.getage();
+    S1.getroll_number();
+    
+    
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
