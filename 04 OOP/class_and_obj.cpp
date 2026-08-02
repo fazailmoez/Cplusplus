8145c364203337e4a66ded672225d473de186df9
Fazail Moez <fazailmoez@gmail.com>
2026-08-02 11:29:03 +0600

    Add class and object program

---
+++ 04 OOP/class_and_obj.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    string name ,grade;
+    int age ,roll_number;
+    
+};
+
+int main()
+{
+    Student S1;
+    S1.name="Rohit";
+    S1.grade="A+";
+    S1.age=20;
+    S1.roll_number=1234;
+    
+    cout<<S1.name<<endl<<S1.grade<<endl<<S1.age<<endl<<S1.roll_number<<endl;
+    
+    
+    Student S2;
+    S2.name="Ahmad";
+    S2.grade="B-";
+    S2.age=12;
+    S2.roll_number=4321;
+    
+    cout<<S2.name<<endl<<S2.grade<<endl<<S2.age<<endl<<S2.roll_number<<endl;
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
